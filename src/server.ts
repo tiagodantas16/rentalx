b4ee182139c6201bdacae15517699dbf49b10b50
Tiago Dantas <tiago.dantas16@live.com>
2021-11-06 18:10:54 -0300

    Criando config do docker e o database

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import swaggerUi from "swagger-ui-express"
 import { router } from "./routes";
 import swagger from "./swagger.json"
-
+import "./database";
 
 const app = express();
 
